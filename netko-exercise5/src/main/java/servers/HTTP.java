bffe617ceb68e0dab6ed832e92074de15ad993a8
Alisa <alfassna@htwg-konstanz.de>
2017-05-01 14:53:46 +0200

    created method to get patient ID from HTTP Header

--- netko-exercise5/src/main/java/servers/HTTP.java
+++ netko-exercise5/src/main/java/servers/HTTP.java
@@ -5,33 +5,31 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HTTP {
-	
+
 	public static void main(String[] args) {
 		HTTP http = new HTTP(8000);
 		http.start();
 	}
-	
+
 	private int port;
-	
+	private String[] lines;
+	private ServerSocket serverSocket;
+
 	public HTTP(int port) {
 		this.port = port;
 	}
-	
+
 	public void start() {
 		try {
-			ServerSocket serverSocket = new ServerSocket(port);
+			serverSocket = new ServerSocket(port);
 			Socket socket = serverSocket.accept();
 			String receivedData = Utilities.receiveData(socket);
-			String[] lines = receivedData.split("\r\n");
+			lines = receivedData.split("\r\n");
 			System.out.println("Empfangene Daten: ");
-			for(String line : lines)
+			for (String line : lines)
 				System.out.println(line);
 			String content = getContent();
-			String response = 
-					"HTTP/1.1 200 OK\r\n"
-					+ "Content-Length: " + content.length() + "\r\n"
-					+ "\r\n"
-					+ content;
+			String response = "HTTP/1.1 200 OK\r\n" + "Content-Length: " + content.length() + "\r\n" + "\r\n" + content;
 			Utilities.sendData(socket, response);
 			serverSocket.close();
 		} catch (IOException e) {
@@ -39,16 +37,25 @@ public class HTTP {
 		}
 	}
 
+	private String getPatientID() {
+		String[] firstLine = lines[0].split(" ");
+		String[] paIDLine = firstLine[1].split("/");
+
+		String paID = paIDLine[2];
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return paID;
+	}
+
 	private String getContent() {
-		String webseite = 
-				  "<html>\r\n"
-				+ 	"<table>\r\n"
-				+ 		"<tr><th>Eigenschaft</th><th>Wert</th></tr>\r\n"
-				+ 		"<tr><td>Vorname</td><td>Kunigunde</td></tr>\r\n"
-				+ 		"<tr><td>Nachname</td><td>Entenmooser</td></tr>\r\n"
-				+ 		"<tr><td>Geburtsdatum</td><td>01.02.1937</td></tr>\r\n"
-				+ 	"</table>\r\n"
-				+ "</html>\r\n";
+		String webseite = "<html>\r\n" + "<table>\r\n" + "<tr><th>Eigenschaft</th><th>Wert</th></tr>\r\n"
+				+ "<tr><td>Vorname</td><td>Kunigunde</td></tr>\r\n"
+				+ "<tr><td>Nachname</td><td>Entenmooser</td></tr>\r\n"
+				+ "<tr><td>Geburtsdatum</td><td>01.02.1937</td></tr>\r\n" + "</table>\r\n" + "</html>\r\n";
 		return webseite;
 	}
+
 }
